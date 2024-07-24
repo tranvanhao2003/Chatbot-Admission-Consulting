21b163798808ca17046922359a957622395b6040
PhamTrinhDuc <duc78240@gmail.com>
2024-07-24 17:32:41 +0700

    update

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
 
-from source.rag.chain_main import RAG
-# from configs.load_config import LoadConfig
-from source.rag_agent import init_agent
+# from source.rag.chain_main import RAG
+# from source.rag_agent import init_agent
+from source.agent.agent_SQL import Agent
+
+agent = Agent()
+
+print(agent.db.run("SELECT diem FROM TVTS WHERE Nganh = 'CÔNG NGHỆ THÔNG TIN'"))
+
 
 # rag = RAG()
 # chain = rag.get_qachain()
 # print(chain.invoke({"question": "cơ hội việc làm cho ngành Kỹ thuật Robot và Trí tuệ nhân tạo"}))
-import time
+# import time
 
-t1 = time.time()
-agent = init_agent()
-# response1 = agent.invoke({"input": "Hello"},
-#                         config={"configurable": {"session_id": "<foo>"}})
-response1 = agent.invoke({"input": "Ngành CNTT lấy bao nhiêu điểm ?"})['output']
-                        # config={"configurable": {"session_id": "<foo>"}})
+# t1 = time.time()
+# agent = init_agent()
+# # response1 = agent.invoke({"input": "Hello"},
+# #                         config={"configurable": {"session_id": "<foo>"}})
+# response1 = agent.invoke({"input": "Ngành CNTT lấy bao nhiêu điểm"})['output']
+#                         # config={"configurable": {"session_id": "<foo>"}})
 
-t2 = time.time() - t1
-print("="* 50 )
-print(response1)
-print(t2)
+# t2 = time.time() - t1
+# print("="* 50 )
+# print(response1)
+# print(t2)
